6fdb7a5f64bb48c365215ddbcf36dde534cb8b37
duranaaron <aaronduran2107@gmail.com>
2025-02-23 14:11:11 +0100

    Small optimisation

--- src/main/java/nl/openminetopia/modules/misc/listeners/PlayerInteractListener.java
+++ src/main/java/nl/openminetopia/modules/misc/listeners/PlayerInteractListener.java
@@ -3,6 +3,7 @@ package nl.openminetopia.modules.misc.listeners;
 import com.jazzkuh.inventorylib.objects.Menu;
 import nl.openminetopia.OpenMinetopia;
 import nl.openminetopia.api.player.PlayerManager;
+import nl.openminetopia.api.player.objects.MinetopiaPlayer;
 import nl.openminetopia.configuration.MessageConfiguration;
 import nl.openminetopia.utils.ChatUtils;
 import org.bukkit.GameMode;
@@ -28,23 +29,14 @@ public class PlayerInteractListener implements Listener {
 
         event.setCancelled(true);
 
-        if (!OpenMinetopia.getDefaultConfiguration().isTrashcanUseDropperInventory()) {
-            new TrashcanMenu(InventoryType.CHEST).open(event.getPlayer());
-        } else {
-            new TrashcanMenu(InventoryType.DROPPER).open(event.getPlayer());
-        }
+        if (!OpenMinetopia.getDefaultConfiguration().isTrashcanUseDropperInventory()) new TrashcanMenu(InventoryType.CHEST).open(event.getPlayer());
+        else new TrashcanMenu(InventoryType.DROPPER).open(event.getPlayer());
 
-        PlayerManager.getInstance().getMinetopiaPlayer(player).whenComplete(((minetopiaPlayer, throwable) -> {
-            if (throwable != null) {
-                throwable.printStackTrace();
-                return;
-            }
-            ChatUtils.sendFormattedMessage(minetopiaPlayer, MessageConfiguration.message("trashcan_message"));
-        }));
+        MinetopiaPlayer minetopiaPlayer = PlayerManager.getInstance().getOnlineMinetopiaPlayer(player);
+        ChatUtils.sendFormattedMessage(minetopiaPlayer, MessageConfiguration.message("trashcan_message"));
     }
 
     private static class TrashcanMenu extends Menu {
-
         public TrashcanMenu(InventoryType type) {
             super(ChatUtils.color(MessageConfiguration.message("trashcan_title")), 3, type, false);
         }
