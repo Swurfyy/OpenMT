f7bad39616578ac55591a355a4755a0c8e63bda2
Svenns <svennijhuis1976@gmail.com>
2025-02-28 03:44:14 +0100

    Added more skript expressions. All the fitness stuff

---
+++ src/main/java/nl/openminetopia/modules/skript/expressions/fitness/ExprFlewMiles.java
@@ -0,0 +1,56 @@
+package nl.openminetopia.modules.skript.expressions.fitness;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import nl.openminetopia.api.player.PlayerManager;
+import nl.openminetopia.api.player.objects.MinetopiaPlayer;
+import org.bukkit.Statistic;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.Nullable;
+
+public class ExprFlewMiles extends SimpleExpression<Integer> {
+
+    static {
+        Skript.registerExpression(ExprFlewMiles.class, Integer.class, ExpressionType.COMBINED, "[the] (omt|openminetopia) fitness flewmiles of %player%");
+    }
+
+    private Expression<Player> player;
+
+    @Override
+    public Class<? extends Integer> getReturnType() {
+        return Integer.class;
+    }
+
+    @Override
+    public boolean isSingle() {
+        return true;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parser) {
+        player = (Expression<Player>) exprs[0];
+        return true;
+    }
+
+    @Override
+    public String toString(@Nullable Event event, boolean debug) {
+        return "Example expression with expression player" + player.toString(event, debug);
+    }
+
+    @Override
+    @Nullable
+    protected Integer[] get(Event event) {
+        Player p = player.getSingle(event);
+        MinetopiaPlayer minetopiaPlayer = PlayerManager.getInstance().getOnlineMinetopiaPlayer(p);
+        if (minetopiaPlayer != null) {
+            return new Integer[] {(minetopiaPlayer.getBukkit().getStatistic(Statistic.AVIATE_ONE_CM) / 1000)};
+        }
+        return null;
+    }
+}
---
+++ src/main/java/nl/openminetopia/modules/skript/expressions/fitness/ExprSecondaryPoints.java
@@ -0,0 +1,58 @@
+package nl.openminetopia.modules.skript.expressions.fitness;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import nl.openminetopia.api.player.PlayerManager;
+import nl.openminetopia.api.player.fitness.FitnessStatisticType;
+import nl.openminetopia.api.player.objects.MinetopiaPlayer;
+import nl.openminetopia.modules.fitness.models.FitnessStatisticModel;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.Nullable;
+
+public class ExprSecondaryPoints extends SimpleExpression<Double> {
+
+    static {
+        Skript.registerExpression(ExprSecondaryPoints.class, Double.class, ExpressionType.COMBINED, "[the] (omt|openminetopia) fitness cheapfood points of %player%");
+    }
+
+    private Expression<Player> player;
+
+    @Override
+    public Class<? extends Double> getReturnType() {
+        return Double.class;
+    }
+
+    @Override
+    public boolean isSingle() {
+        return true;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parser) {
+        player = (Expression<Player>) exprs[0];
+        return true;
+    }
+
+    @Override
+    public String toString(@Nullable Event event, boolean debug) {
+        return "Example expression with expression player" + player.toString(event, debug);
+    }
+
+    @Override
+    @Nullable
+    protected Double[] get(Event event) {
+        Player p = player.getSingle(event);
+        MinetopiaPlayer minetopiaPlayer = PlayerManager.getInstance().getOnlineMinetopiaPlayer(p);
+        FitnessStatisticModel eatingStatistic = minetopiaPlayer.getFitness().getStatistic(FitnessStatisticType.EATING);
+        if (minetopiaPlayer != null) {
+            return new Double[] {eatingStatistic.getSecondaryPoints()};
+        }
+        return null;
+    }
+}
