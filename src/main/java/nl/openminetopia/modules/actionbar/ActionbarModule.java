35fc18d28cc49bdcd3cc719adaf594a412d00f17
Ruubske <rubendekeizer@gmail.com>
2025-09-05 15:12:25 +0200

    found another player runnable

--- src/main/java/nl/openminetopia/modules/actionbar/ActionbarModule.java
+++ src/main/java/nl/openminetopia/modules/actionbar/ActionbarModule.java
@@ -2,13 +2,20 @@ package nl.openminetopia.modules.actionbar;
 
 import com.jazzkuh.modulemanager.spigot.SpigotModule;
 import com.jazzkuh.modulemanager.spigot.SpigotModuleManager;
+import lombok.Getter;
 import nl.openminetopia.OpenMinetopia;
+import nl.openminetopia.api.player.PlayerManager;
 import nl.openminetopia.modules.actionbar.commands.ActionbarCommand;
 import nl.openminetopia.modules.actionbar.listeners.ActionbarJoinListener;
+import nl.openminetopia.modules.actionbar.runnables.ActionbarRunnable;
 import nl.openminetopia.modules.data.DataModule;
 import org.jetbrains.annotations.NotNull;
 
 public class ActionbarModule extends SpigotModule<@NotNull OpenMinetopia> {
+
+    @Getter
+    private ActionbarRunnable actionbarRunnable;
+
     public ActionbarModule(SpigotModuleManager<@NotNull OpenMinetopia> moduleManager, DataModule dataModule) {
         super(moduleManager);
     }
@@ -17,5 +24,13 @@ public class ActionbarModule extends SpigotModule<@NotNull OpenMinetopia> {
     public void onEnable() {
         registerComponent(new ActionbarJoinListener());
         registerComponent(new ActionbarCommand());
+
+        this.actionbarRunnable = new ActionbarRunnable(PlayerManager.getInstance(), 1000, 100, 2500, () -> PlayerManager.getInstance().getOnlinePlayers().keySet().stream().toList());
+        OpenMinetopia.getInstance().registerDirtyPlayerRunnable(actionbarRunnable, 20L);
+    }
+
+    @Override
+    public void onDisable() {
+        OpenMinetopia.getInstance().unregisterDirtyPlayerRunnable(actionbarRunnable);
     }
 }
---
+++ src/main/java/nl/openminetopia/modules/actionbar/runnables/ActionbarRunnable.java
@@ -0,0 +1,33 @@
+package nl.openminetopia.modules.actionbar.runnables;
+
+import nl.openminetopia.OpenMinetopia;
+import nl.openminetopia.api.player.PlayerManager;
+import nl.openminetopia.api.player.objects.MinetopiaPlayer;
+import nl.openminetopia.configuration.DefaultConfiguration;
+import nl.openminetopia.framework.runnables.AbstractDirtyRunnable;
+import nl.openminetopia.utils.ChatUtils;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.function.Supplier;
+
+public class ActionbarRunnable extends AbstractDirtyRunnable<UUID> {
+
+    private final PlayerManager playerManager;
+
+    public ActionbarRunnable(PlayerManager playerManager, long minIntervalMs, int batch, long heartbeatMs, Supplier<List<UUID>> allKeysSupplier) {
+        super(minIntervalMs, batch, heartbeatMs, allKeysSupplier);
+        this.playerManager = playerManager;
+    }
+
+    @Override
+    protected void process(UUID key) {
+        DefaultConfiguration configuration = OpenMinetopia.getDefaultConfiguration();
+        if (!configuration.isActionbarEnabled()) return;
+        MinetopiaPlayer minetopiaPlayer = playerManager.getOnlinePlayers().get(key);
+        if (minetopiaPlayer == null) return;
+        if (!minetopiaPlayer.isActionbarVisible()) return;
+        Objects.requireNonNull(minetopiaPlayer.getBukkit().getPlayer()).sendActionBar(ChatUtils.format(minetopiaPlayer, configuration.getActionbarText()));
+    }
+}
